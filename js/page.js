bd29e5f234879dce16f2acdcd64d54dd27ead438
colorlessenergy <brianmunozdev@gmail.com>
2017-11-18 06:57:08 -0800

    added a design

--- js/page.js
+++ js/page.js
@@ -4,5 +4,12 @@ let send = document.querySelector("#send");
 
 send.addEventListener("click", function () {
   let answer = high(input.value);
-  output.innerHTML = answer.word + " has the biggest value which is " + answer.value ;
+  output.innerHTML = answer.word + " has the biggest value which is " + answer.value;
+  input.value = "";
 });
+
+input.addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    send.click();
+  }
+})
